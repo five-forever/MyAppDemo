3b471c107bebfd178d04fda088815fbcb9ab7cfc
TalosGuo <talosguo@vesync.com.cn>
2021-09-16 15:03:36 +0800

    测试

---
+++ MyAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAppDemo
+//
+//  Created by 郭韬 on 2021/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
